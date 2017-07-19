c5184c25e25315f1f1699df3c429a8b0eeee864f
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-07-19 21:56:31 +0200

    Add deadline date update

--- js/core.js
+++ js/core.js
@@ -2,6 +2,19 @@ var uid;
 
 var newDeadline = {}
 
+function updateDeadlines() {
+  var now = _.now()
+  var deadlines = document.getElementsByClassName('deadline')
+  _.forEach(deadlines, function(deadline) {
+    console.log('WOW')
+    var end_date_field = deadline.getElementsByClassName('end_date')[0]
+    end_date_field.innerHTML = deadline.getAttribute('end_date') - now
+    console.log(end_date_field)
+    console.log(deadline.getAttribute('end_date') - now)
+  })
+}
+setInterval(updateDeadlines, 100)
+
 function getNewDeadlineDate() {
   var day = document.getElementById('deadline-day').value
   var month = document.getElementById('deadline-month').value
@@ -29,19 +42,21 @@ function initalizeApp() {
 
   firebase.database().ref('deadlines/' + uid).on('child_added', function(data) {
     var newDeadline = document.createElement('li')
-    newDeadline.innerHTML = data.val().task + '<div class="date">' + data.val().end_date + '</div>'
+    newDeadline.classList.add('deadline')
+    newDeadline.id = data.key
+    newDeadline.setAttribute('end_date', data.val().end_date)
+    newDeadline.innerHTML = data.val().task + '<div class="end_date"></div>'
     document.getElementById('deadlines').prepend(newDeadline)
   });
 }
 
 function addDeadline() {
   var task = document.getElementById('deadline-task').value
-  var now = new Date()
   var end_date = getNewDeadlineDate()
 
   firebase.database().ref('deadlines/' + uid).push().set({
     task: task,
-    creation_date: now.getTime(),
+    creation_date: _.now(),
     end_date: end_date.getTime()
   })
 }
