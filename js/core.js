98b0930227ebb4b308c0a8d8efa87ec723849fee
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-07-19 21:34:11 +0200

    Reorganize add new deadline process

--- js/core.js
+++ js/core.js
@@ -1,13 +1,19 @@
 var uid;
 
-function updateDeadlineDate() {
+var newDeadline = {}
+
+function getNewDeadlineDate() {
   var day = document.getElementById('deadline-day').value
   var month = document.getElementById('deadline-month').value
   var year = document.getElementById('deadline-year').value
   var hour = document.getElementById('deadline-hour').value
   var minute = document.getElementById('deadline-minute').value
 
-  var date = new Date(year, month - 1, day, hour, minute)
+  return new Date(year, month - 1, day, hour, minute)
+}
+
+function updateNewDeadlineDate() {
+  var date = getNewDeadlineDate()
   document.getElementById('deadline-date-display').innerHTML = date.toJSON()
 }
 
@@ -15,11 +21,11 @@ function initalizeApp() {
   document.getElementById('landing').classList.remove('active')
   document.getElementById('app').classList.add('active')
 
-  document.getElementById('deadline-day').addEventListener("input", updateDeadlineDate, false);
-  document.getElementById('deadline-month').addEventListener("input", updateDeadlineDate, false);
-  document.getElementById('deadline-year').addEventListener("input", updateDeadlineDate, false);
-  document.getElementById('deadline-hour').addEventListener("input", updateDeadlineDate, false);
-  document.getElementById('deadline-minute').addEventListener("input", updateDeadlineDate, false);
+  document.getElementById('deadline-day').addEventListener("input", updateNewDeadlineDate, false);
+  document.getElementById('deadline-month').addEventListener("input", updateNewDeadlineDate, false);
+  document.getElementById('deadline-year').addEventListener("input", updateNewDeadlineDate, false);
+  document.getElementById('deadline-hour').addEventListener("input", updateNewDeadlineDate, false);
+  document.getElementById('deadline-minute').addEventListener("input", updateNewDeadlineDate, false);
 
   firebase.database().ref('deadlines/' + uid).on('child_added', function(data) {
     var newDeadline = document.createElement('li')
@@ -30,11 +36,13 @@ function initalizeApp() {
 
 function addDeadline() {
   var task = document.getElementById('deadline-task').value
-  var date = document.getElementById('deadline-date').value
+  var now = new Date()
+  var end_date = getNewDeadlineDate()
+
   firebase.database().ref('deadlines/' + uid).push().set({
     task: task,
-    creation_date: new Date().toJSON(),
-    end_date: new Date()
+    creation_date: now.getTime(),
+    end_date: end_date.getTime()
   })
 }
 
