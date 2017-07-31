68d4fa4567a1a27d23a4335c1bc50cdbcfa1a704
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-07-31 19:00:46 +0200

    Add base for notification system

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+var config = {
+  apiKey: "AIzaSyAQH_mzKLGnD7a-MubUxR-zz0AmNGpH2PQ",
+  authDomain: "workdone-160900.firebaseapp.com",
+  databaseURL: "https://workdone-160900.firebaseio.com",
+  projectId: "workdone-160900",
+  storageBucket: "workdone-160900.appspot.com",
+  messagingSenderId: "753972019068"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
